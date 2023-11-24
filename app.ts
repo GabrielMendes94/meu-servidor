213ffea4a9b5b10bef75d9cf2f34f000de5cb6b8
Gabriel Mendes <103916287+GabrielMendes94@users.noreply.github.com>
2023-11-24 19:18:56 +0000

    post, put e delete

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const livros = [
+let livros = [
     { id: 1, titulo: "Aprendendo TypeScript", autor: "John Doe" },
     { id: 2, titulo: "Node.js para Iniciantes", autor: "Jane Smith" },
 ];
@@ -28,6 +28,56 @@ app.get("/livros/:id", (req: Request, res: Response) => {
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Bem-vindo ao meu servidor!");
+});
+
+app.post("/livros", (req: Request, res: Response) => {
+    const novoLivro = {
+        id: livros.length + 1,
+        titulo: req.body.titulo,
+        autor: req.body.autor,
+    };
+
+    livros.push(novoLivro);
+    res.status(201).json(novoLivro);
+});
+
+app.put("/livros/:id", (req: Request, res: Response) => {
+
+    const livroIndex = livros.findIndex((l) => l.id === parseInt(req.params.id));
+
+    if (livroIndex === -1) {
+        return res.status(404).json({ mensagem: "Livro não encontrado" });
+    }
+
+    livros[livroIndex] = {
+
+        id: livros[livroIndex].id,
+        titulo: req.body.titulo,
+        autor: req.body.autor,
+
+    };
+
+    res.json(livros[livroIndex]);
+
+    /*
+    const livro = {
+        id: parseInt(req.params.id),
+        titulo: req.body.titulo,
+        autor: req.body.autor,
+    };
+
+    const livroIndex = livros.findIndex((l) => l.id === parseInt(req.params.id));
+
+    livros[livroIndex] = livro;
+
+    res.status(201).json(livro);
+    *///versaoprof
+
+});
+
+app.delete("/livros/:id", (req: Request, res: Response) => {
+    livros = livros.filter((l) => l.id !== parseInt(req.params.id));
+    res.json({ mensagem: "Livro removido com sucesso" });
 });
 
 const porta: number = 3000;
